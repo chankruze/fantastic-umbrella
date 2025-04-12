a6da8c865d435626bb21859c9edb1a2aba45ede7
chankruze <chankruze@gmail.com>
2025-04-12 23:03:35 +0530

    refactor: cleanup
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/Movies/constants.js
+++
@@ -1,3 +0,0 @@
-export const FALLBACK_IMAGE = "https://dancyflix.com/placeholder.png";
-export const DEFAULT_PAGE_SIZE = 10;
-export const DEFAULT_PAGE_NUMBER = 1;
--- src/components/commons/index.js
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import PageNotFound from "./PageNotFound";
-import TooltipWrapper from "./TooltipWrapper";
-
-export { Header, PageNotFound, TooltipWrapper };
